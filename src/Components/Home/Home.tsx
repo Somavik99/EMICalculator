080cab8e0413cddda64665f69653200e7c77ee86
Avik Roy <somavik99@gmail.com>
2025-05-05 19:48:20 +0530

    EMI Calculator

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,42 @@
+import ResponsiveAppBar from "../Navbar/NavBar";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+const Home = () => {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <div className="home_container">
+        <div style={{ fontSize: "40px" }}>Loan Calculator Dashboard</div>
+        <Box
+          component="form"
+          sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Loan Amount"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-basic"
+            label="Interest Rate(%)"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-basic"
+            label="Terms(yrs)"
+            variant="outlined"
+          />
+        </Box>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained">CALCULATE</Button>
+        </Stack>
+      </div>
+    </>
+  );
+};
+
+export default Home;
